054c78d0cd0d124a1c9a33f79add73fba149e2e5
saito-dat <kuroutoFOV90@gmail.com>
2022-06-23 14:53:55 +0900

    Initial commit

---
+++ v0623/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0623
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -10;
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -10;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("葛西達也");
+            timer1.Enabled = false;
+        }
+    }
+}
